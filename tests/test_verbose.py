cad4e68d691475f7948333de2c977463a077edd2
SkillHeaven <37280159+SkillHeaven@users.noreply.github.com>
2025-07-30 10:33:21 -0700

    Fix verbose logging cleanup

---
+++ tests/test_verbose.py
@@ -0,0 +1,11 @@
+import sys
+from runepy.verbose import enable, disable
+
+
+def test_verbose_enable_disable():
+    enable()
+    try:
+        assert sys.gettrace() is not None
+    finally:
+        disable()
+    assert sys.gettrace() is None
